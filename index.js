ec08552e20b7b57e09bcaf469e2f3d562cc47f21
Vigneshkalithas <96053261+Vigneshkalithas@users.noreply.github.com>
2021-12-24 15:04:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+console.log('hi')
+console.log(4+5)
+var t3="5" - 3;
+console.log(t3);
+console.log(typeof t3);
+var v1=4
+var v2=4;
+var y1=[100,200]
+var y2=[100,200];
+var y3=y1;
+
+console.log(v1===v2)
+console.log(y1===y2)
+console.log(y1===y3)
+
+
+
+
+let marks = [40,90,80,20,30];
+
+for (let i=0;i<marks.length;i++){
+    console.log(marks[i])
+}
+
+
+
+
+console.log("before",z1);
+var z1 = 10;
+console.log("after",z1);
+
+
+
+
+
+//funcion fullname(firstname , lastname){
+  //  return "welcome" lastname, firstname !!!
+//}
+//console.log(full name("Vijaya", Bharathy))
+
+
+
+
+
+
+// [32, 57, 22]
+// Task 1
+// Print the name list - As an array
+// Expected Output
+// ["Yvette Merritt", "Lillian Ellis", "Mccall Carter", "Pate Collier" ,
+//  "Debra Beard", "Nettie Hancock",  "Hatfield Hodge"]
+
+// Task 2
+// >=40 pass.. find those student that passed (Use array method)
+// Expected Output
+// [{
+//     marks: 57,
+//     name: "Lillian Ellis"
+//   },
+//     {
+//     marks: 91,
+//     name: "Debra Beard"
+//   },
+//   {
+//     marks: 75,
+//     name: "Nettie Hancock"
+//   }]
+
+// Task 3
+//  Find all the names who failed the exams (Array methods)
+// Expected Output
+//   ["Yvette Merritt",
+//   "Mccall Carter",
+//   "Pate Collier",
+//   "Hatfield Hodge"
+// ];
+
+const scores = [
+  {
+    marks: 32,
+    name: "Yvette Merritt"
+  },
+  {
+    marks: 57,
+    name: "Lillian Ellis"
+  },
+  {
+    marks: 22,
+    name: "Mccall Carter"
+  },
+  {
+    marks: 21,
+    name: "Pate Collier"
+  },
+  {
+    marks: 91,
+    name: "Debra Beard"
+  },
+  {
+    marks: 75,
+    name: "Nettie Hancock"
+  },
+  {
+    marks: 20,
+    name: "Hatfield Hodge"
+  }
+];
+//const NewMsg = ({name}) => `${name}`;
+//console.log(NewMsg(scores));
+//function dubble({name})
+//reurn doble
+//console.log(scores.filter((name)=>name;
+console.log(scores.filter((name)=>name;
